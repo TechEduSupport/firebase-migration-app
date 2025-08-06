623f9c121923f3cfbc340fa1cd412945d0666b6d
takutakutack <ewige.liebe,yt@gmail.com>
2025-08-06 10:41:34 +0900

    Fix: Unify Firebase SDK to v8 compat

--- public/firebase-init.js
+++ public/firebase-init.js
@@ -1,15 +1,16 @@
-// Your web app's Firebase configuration
+// あなたのウェブアプリのFirebase設定
+// TODO: この部分はあなたのプロジェクトの情報に書き換えてください
 const firebaseConfig = {
   apiKey: "AIzaSyBmZF5y9z8H8CsMtTgNwo50d7qOr6jdIec",
   authDomain: "tsa-0503.firebaseapp.com",
-  databaseURL: "https://tsa-0503-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "tsa-0503",
   storageBucket: "tsa-0503.appspot.com",
   messagingSenderId: "283742763295",
-  appId: "1:283742763295:web:ed8c68433ecbe303bad82a",
-  measurementId: "G-3EKFPLWBMR"
+  appId: "1:283742763295:web:ed8c68433ecbe303bad82a"
 };
 
-// Initialize Firebase
+// Firebaseを初期化
 firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth(); // Firebase Authenticationのインスタンスを取得
+
+// 他のファイルで `auth` という変数を使えるように定義
+const auth = firebase.auth();
